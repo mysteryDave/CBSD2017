521be562a71ca69a8d4737f93fd61bca54de48d1
mysteryDave <mystery.dave@yahoo.com>
2017-05-18 18:39:22 +0100

    Created sample classes for carriers and airports to ease creation of test flights

---
+++ src/main/java/net/java/cargotracker/domain/model/airport/SampleAirports.java
@@ -0,0 +1,60 @@
+package net.java.cargotracker.domain.model.airport;
+
+import net.java.cargotracker.domain.model.location.Location;
+import net.java.cargotracker.domain.model.location.UnLocode;
+
+/**
+ * Sample locations, for test purposes.
+ */
+public class SampleAirports { //50 Sample airports
+    public static final Airport ATLANTA = new Airport("ATL", "Atlanta Hartsfield-Jackson");
+    public static final Airport DUBAI = new Airport("DXB", "Dubai International");
+    public static final Airport GATWICK = new Airport("LGW", "London Gatwick");
+    public static final Airport HEATHROW = new Airport("LHR", "London Heathrow");
+    public static final Airport HONG_KONG = new Airport("HKG", "Hong Kong International");
+    public static final Airport LOS_ANGELES = new Airport("LAX", "Los Angeles International");
+    public static final Airport LONDON_CITY = new Airport("LCY", "London City");
+    public static final Airport LUTON = new Airport("LTN", "London Luton");
+    public static final Airport STANSTED = new Airport("STN", "London Stansted");
+    public static final Airport SOUTHEND = new Airport("SEN", "Southend");
+    public static final Airport BIRMINGHAM = new Airport("BHX", "Birmingham International");
+    public static final Airport MANCHESTER = new Airport("MAN", "Manchester");
+    public static final Airport EDINBURGH = new Airport("EDI", "Edinburgh");
+    public static final Airport GLASGOW = new Airport("GLA", "Glasgow International");
+    public static final Airport BRISTOL = new Airport("BRS", "Bristol International");
+    public static final Airport NEWCASTLE = new Airport("NCL", "Newcastle");
+    public static final Airport EAST_MIDS = new Airport("EMA", "East Midlands");
+    public static final Airport BELFAST_INTL = new Airport("BFS", "Belfast International");
+    public static final Airport LIVERPOOL = new Airport("LPL", "Liverpool John Lennon");
+    public static final Airport LEEDS = new Airport("LBA", "Leeds Bradford");
+    public static final Airport ABERDEEN = new Airport("ABZ", "Aberdeen Dyce");
+    public static final Airport BELFAST_GB = new Airport("BHD", "George Best Belfast City");
+    public static final Airport SOUTHAMPTON = new Airport("SOU", "Southampton");
+    public static final Airport CARDIFF = new Airport("CWL", "Cardiff International");
+    public static final Airport CHICAGO = new Airport("ORD", "Chicago O'Hare International");
+    public static final Airport DALLAS = new Airport("DFW", "Dallas Fort Worth");
+    public static final Airport DENVER = new Airport("DEN", "Denver International");
+    public static final Airport NEW_YORK_JFK = new Airport("JFK", "New York John F Kennedy");
+    public static final Airport SAN_FRANSISCO = new Airport("SFO", "San Fransisco International");
+    public static final Airport CHARLOTTE = new Airport("CLT", "Charlotte Douglas");
+    public static final Airport LAS_VEGAS = new Airport("LAS", "Las Vegas McCarran");
+    public static final Airport PHOENIX = new Airport("PHX", "Phoenix Sky Harbour");
+    public static final Airport BEIJING = new Airport("PEK", "Beijing Capital");
+    public static final Airport TOKYO = new Airport("HND", "Tokyo International");
+    public static final Airport PARIS_CDG = new Airport("CDG", "Paris Charles de Gaulle");
+    public static final Airport ISTANBUL = new Airport("IST", "Istanbul Ataturk");
+    public static final Airport FRANKFURT = new Airport("FRA", "Frankfurt am Main");
+    public static final Airport AMSTERDAM = new Airport("AMS", "Amsterdam Schiphol");
+    public static final Airport SINGAPORE = new Airport("SIN", "Singapore Changi");
+    public static final Airport GUANGZHOU = new Airport("CAN", "Guangzhou Baiyun");
+    public static final Airport JAKARTA = new Airport("CGK", "Jakarta Soekarno-Hatta");
+    public static final Airport BANGKOK = new Airport("BKK", "Bangkok Suvarnabhumi");
+    public static final Airport MADRID = new Airport("MAD", "Madrid Barajas");
+    public static final Airport MUNICH = new Airport("MUC", "Munich International");
+    public static final Airport ROME = new Airport("FCO", "Rome Fiumicino Leonardo Da Vinci");
+    public static final Airport BARCELONA = new Airport("BCN", "Barcelona International");
+    public static final Airport SYDNEY = new Airport("SYD", "Sydney Kingsford Smith");
+    public static final Airport MUMBAI = new Airport("BOM", "Mumbai Chhatrapati Shivaji");
+    public static final Airport SAU_PAULO = new Airport("GRU", "Sau Paulo Guarulhos");
+    public static final Airport MEXICO = new Airport("MEX", "Mexico Benito Juarez");
+}
---
+++ src/main/java/net/java/cargotracker/domain/model/carrier/SampleCarriers.java
@@ -0,0 +1,37 @@
+package net.java.cargotracker.domain.model.carrier;
+
+/**
+ * Sample locations, for test purposes.
+ */
+public class SampleCarriers { //30 Sample airlines
+    public static final Carrier EASYJET = new Carrier("U2", "Easyjet Airline Company");
+    public static final Carrier BA = new Carrier("BA", "British Airways");
+    public static final Carrier FLYBE = new Carrier("BE", "Flybe");
+    public static final Carrier VIRGIN = new Carrier("VS", "Virgin Atlantic");
+    public static final Carrier MONARCH = new Carrier("ZB", "Monarch");
+    public static final Carrier JET2 = new Carrier("LS", "Jet2.com");
+    public static final Carrier THOMAS_COOK = new Carrier("MT", "Thomas Cook Airlines");
+    public static final Carrier LOGAN = new Carrier("LM", "Loganair");
+    public static final Carrier AURIGNY = new Carrier("GR", "Aurigny Air Services");
+    public static final Carrier THOMSON = new Carrier("BY", "Thomson Airways");
+    public static final Carrier RYANAIR = new Carrier("FR", "Ryanair");
+    public static final Carrier LUFTHANSA = new Carrier("LH", "Deutsche Lufthansa");
+    public static final Carrier IBERIA = new Carrier("IB", "Iberia Lineas Aereas de Espana");
+    public static final Carrier AIR_FRANCE = new Carrier("AF", "Air France");
+    public static final Carrier KLM = new Carrier("KL", "KLM Royal Dutch Airlines");
+    public static final Carrier TURKISH = new Carrier("TK", "Turkish Airlines");
+    public static final Carrier AEROFLOT = new Carrier("SU", "PJSC Aeroflot");
+    public static final Carrier SAS = new Carrier("SK", "Scandanavian Airlines System (SAS)");
+    public static final Carrier NORWEGIAN = new Carrier("DY", "Norwegian Air Shuttle AS");
+    public static final Carrier AIR_BERLIN = new Carrier("AB", "Air Berlin & Co Luftverkerhs KG");
+    public static final Carrier EMIRATES = new Carrier("EK", "Emirates");
+    public static final Carrier UNITED = new Carrier("UA", "United Airlines");
+    public static final Carrier EAST_CHINA = new Carrier("MU", "China Eastern Airlines");
+    public static final Carrier AIR_CANADA = new Carrier("AC", "Air Canada");
+    public static final Carrier ANA = new Carrier("NH", "All Nippon Airways Co");
+    public static final Carrier AA = new Carrier("AA", "American Airlines Inc");
+    public static final Carrier SOUTHWEST = new Carrier("WN", "Southwest Airlines Co");
+    public static final Carrier DELTA = new Carrier("DL", "Delta Air Lines Inc");
+    public static final Carrier SOUTH_CHINA = new Carrier("CZ", "China Southern Airlines");
+    public static final Carrier AIR_CHINA = new Carrier("CA", "Air China");
+}
