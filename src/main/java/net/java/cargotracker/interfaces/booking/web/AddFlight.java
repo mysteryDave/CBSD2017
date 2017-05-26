0fb82c8562c40652af9082c8fcacc4860075779c
David Tucker <dtucke03@dcs.bbk.ac.uk>
2017-05-26 02:30:34 +0100

    added autocomplete function to add flight dialog

--- src/main/java/net/java/cargotracker/interfaces/booking/web/AddFlight.java
+++ src/main/java/net/java/cargotracker/interfaces/booking/web/AddFlight.java
@@ -62,15 +62,58 @@ public class AddFlight implements Serializable {
 
     public List<String> autoCarrierCode(String query) {
         List<Carrier> airlines = airService.listCarriers();
-        List<String> airlineCodes = new ArrayList<>(airlines.size());
-        for (Carrier airline : airlines) { airlineCodes.add(airline.getCode()); }
+        List<String> airlineCodes;
+
+        switch (query.length()) {
+            case 0: //Nothing typed return all airline codes.
+                airlineCodes = new ArrayList<>(airlines.size());
+                for (Carrier airline : airlines) { airlineCodes.add(airline.getCode()); }
+                break;
+            case 1: //One or two characters is likely to be a code.
+            case 2: //Return matches against code and matches against first letter(s) of airport name.
+                airlineCodes = new ArrayList<>(airlines.size()/10);
+                for (Carrier airline : airlines) {
+                    if (airline.getCode().contains(query.toUpperCase()) || airline.getName().substring(0,query.length()).toUpperCase().matches(query.toUpperCase())) {
+                        airlineCodes.add(airline.getCode());
+                    }
+                }
+                break;
+            default: //More than two characters cannot be a code, match against airport name only.
+                airlineCodes = new ArrayList<>(airlines.size()/20);
+                for (Carrier airline: airlines) {
+                    if (airline.getName().toUpperCase().contains(query.toUpperCase())) airlineCodes.add(airline.getCode());
+                }
+        }
+
         return airlineCodes;
     }
 
     public List<String> autoPortCode(String query) {
         List<Airport> ports = airService.listAirports();
-        List<String> portCodes = new ArrayList<>(ports.size());
-        for (Airport airport : ports) { portCodes.add(airport.getCode()); }
+        List<String> portCodes;
+
+        switch (query.length()) {
+            case 0:
+                portCodes = new ArrayList<>(ports.size());
+                for (Airport port : ports) { portCodes.add(port.getCode()); }
+                break;
+            case 1:
+            case 2:
+            case 3:
+                portCodes = new ArrayList<>(ports.size()/10);
+                for (Airport port : ports) {
+                    if (port.getCode().contains(query.toUpperCase()) || port.getName().substring(0,query.length()).toUpperCase().matches(query.toUpperCase())) {
+                        portCodes.add(port.getCode());
+                    }
+                }
+                break;
+            default:
+                portCodes = new ArrayList<>(ports.size()/20);
+                for (Airport port: ports) {
+                    if (port.getName().toUpperCase().contains(query.toUpperCase())) portCodes.add(port.getCode());
+                }
+        }
+
         return portCodes;
     }
 
